d7568d4465e3e05c8e3a01765742cd8659b501c1
DESKTOP-OD98C9J\Bobby <bobbyras90@gmail.com>
2017-09-13 14:29:52 -0700

    Added an infinite loop.

--- WebServer.java
+++ WebServer.java
@@ -9,24 +9,26 @@ public class WebServer
 	public static void main(String args[]) throws IOException 
 	{
 		ServerSocket serverSocket = new ServerSocket(8080);
-		Socket socket = serverSocket.accept();
-		Scanner scanner = new Scanner(socket.getInputStream());
-		String message = scanner.next();
-		System.out.println("Message: " + message);
-		
-		String filename = message.split(" ")[1];
-		System.out.println("Filename: " + filename);
-		
-		try {
-			OutputStream clientStream = socket.getOutputStream();
-			Files.copy(filename, clientStream);
-			clientStream.flush();
-		} catch(IOException e) {
-			PrintStream p = new PrintStream(socked.getOutputStream());
-			p.println("Error: 400");
-		}
-		
-		scanner.close();
+		while (true) {
+            Socket socket = serverSocket.accept();
+            Scanner scanner = new Scanner(socket.getInputStream());
+            String message = scanner.next();
+            System.out.println("Message: " + message);
+
+            String filename = message.split(" ")[1];
+            System.out.println("Filename: " + filename);
+
+            try {
+                OutputStream clientStream = socket.getOutputStream();
+                Files.copy(filename, clientStream);
+                clientStream.flush();
+            } catch (IOException e) {
+                PrintStream p = new PrintStream(socked.getOutputStream());
+                p.println("Error: 400");
+            }
+
+            scanner.close();
+        }
 		serverSocket.close();
 	}
 }
