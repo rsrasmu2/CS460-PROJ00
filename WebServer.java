11e9fab288ba6621c8804a6a5bdebe00eb60814b
KahnAlexander <kahn.l.alexander@gmail.com>
2017-09-13 13:59:27 -0700

    First try at Proj00, did top portion of python file, still need to figure out sending to client

---
+++ WebServer.java
@@ -0,0 +1,29 @@
+import java.net.Socket;
+import java.util.Scanner;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.net.ServerSocket;
+
+public class WebServer
+{
+	public static void main(String args[]) throws IOException 
+	{
+		ServerSocket serverSocket = new ServerSocket(8080);
+		Socket socket = serverSocket.accept();
+		Scanner scanner = new Scanner(socket.getInputStream());
+		String message = scanner.next();
+		System.out.println("Message: " + message);
+		
+		String filename = message.split(" ")[1];
+		System.out.println("Filename: " + filename);
+		
+		try {
+			PrintStream output = new PrintStream(socket.getOutputStream());
+		} catch(IOException e) {
+			System.out.println(e.getMessage());
+		}
+		
+		scanner.close();
+		serverSocket.close();
+	}
+}
