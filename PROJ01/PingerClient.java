e0e039f6a871fa72d58fd865c2531d79cff6c0c1
ChandlerAHayes <ChandlerAHayes@gmail.com>
2017-09-23 16:04:14 -0700

    Add files via upload
    Fixed a few things like converting nanoseconds to seconds properly and catching ArrayIndexOutOfBoundsException for the command line arguments

--- PROJ01/PingerClient.java
+++ PROJ01/PingerClient.java
@@ -1,4 +1,5 @@
 import java.net.*;
+import java.rmi.server.ExportException;
 import java.text.SimpleDateFormat;
 import java.util.Locale;
 import java.util.TimeZone;
@@ -8,42 +9,38 @@ import java.lang.Thread;
 import java.lang.IllegalArgumentException;
 
 /*
-* Authors: Chandler Hayes, Dillan Mills, Alex Kahn, Robert Rasmussen, Josh Shaffer, and Summer Stapleton
+* Authors: Chandler Hayes, Alex Kahn, Robert Rasmussen, Josh Shaffer, and Summer Stapleton
 *
 * This sends 10 pings to a corresponding server and reports the RTTs of each
-* individual ping. It also reports the maximum, minimum, and average RTT of the set of
-* pings sent.
+* individual ping. It also reports the maximum and minimum RTT of the set of
+* pings sent, and the average RTT of the set of pings sent.
 * */
 
 public class PingerClient {
     public static void main(String args[]) {
-    	DatagramSocket socket;  //creates a UDP socket
+        DatagramSocket socket;  //creates a UDP socket
         InetAddress address;
         byte[] buf;     //stores the message for the packet
         int timeout = 1000;     //it's in milliseconds, which is equal to 1 second
         long startTime;     //time when packet is sent to server, in nanoseconds
         long endTime;   //time when server sends a packet back, in nanoseconds
         double RTT;     //Round Trip Time
-        
-        //Holds the round trip time of every packet that was successfully sent back
-        ArrayList<Double> RTTList = new ArrayList<Double>();
 
-        //Getting the address specified from the command line
-        String host = args[0];  //getting the hostname
-        String port = args[1];  //getting the port number
+        try{
+            //Holds the round trip time of every packet that was successfully sent back
+            ArrayList<Double> RTTList = new ArrayList<Double>();
 
-        try {
-        	/*
-        	****** DatagramSocket(int port, InetAddress laddr) ??? *******
-        	*   Should we use the above constructuor?
-        	*/
+            //Getting the address specified from the command line
+            String host = args[0];  //getting the hostname
+            String port = args[1];  //getting the port number
+
+            //Creating UDP socket
             socket = new DatagramSocket();  //creating a UDP socket connection
-        	socket.setSoTimeout(timeout);   //sets how long the socket will wait for a response from server
-	        address = InetAddress.getByName(host);  //gets the ip address of the server
+            socket.setSoTimeout(timeout);   //sets how long the socket will wait for a response from server
+            address = InetAddress.getByName(host);  //gets the ip address of the server
 
             //this loop will send a ping to the server for every iteration
-	        for(int i = 0; i < 10; i++) {
-	        	
+            for(int i = 0; i < 10; i++) {
                 //below is getting the date & time as a string to store in the packet that will be sent to the server
                 Date date = new Date(System.currentTimeMillis());   //current date & time
                 SimpleDateFormat sdf = new SimpleDateFormat("EE MMM d hh:mm:ss yyyy", Locale.ENGLISH);  //setting format
@@ -63,13 +60,13 @@ public class PingerClient {
                 try {
                     socket.receive(packet);     //receive packet
                     endTime = System.nanoTime();    //time we received the packet at
-                    RTT = (endTime - startTime) / 1000000000000.0;    //calculating RTT
+                    RTT = (endTime - startTime) / 1000000000.0;    //calculating RTT
                     RTTList.add(RTT);   //storing this specific RTT to our list of RTT's
                     String received = new String(
-                            packet.getData(), 0, packet.getLength());   //string version of the bytes received from the packet
+                            packet.getData(), 0, packet.getLength());   //string version of the bytes received from teh packet
 
                     //Printing reply & the RTT for the specific ping
-                    System.out.print("Reply from " + host + ": " + received);   //printing reply
+                    System.out.print("\nReply from " + host + ": " + received);   //printing reply
                     System.out.print("RTT: ");  //printing RTT
                     System.out.format("%.12f", RTT);    //formating the number for RTT
                     System.out.println();
@@ -78,28 +75,39 @@ public class PingerClient {
                 catch (SocketTimeoutException e) {
                     System.out.println("Request timed out.");
                 }
+                catch(ArrayIndexOutOfBoundsException e)
+                {
+                    System.out.println("Missing arguments");
+                }
             }
 
             //Below is calculating the min, max, and average of all the RTT for all the succesful pings
-	        double min = 1000000000;
-	        double max = 0;
-	        double avg = 0;
 
-	        //going through the list to calculate the min, max, and sum of all the RTT's
-	        for(double d : RTTList) {
-	        		if(d < min) min = d;
-	        		if(d > max) max = d;
-	        		avg += d;   //summing all the RTT's so we can find the average
-	        }
-	        avg /= RTTList.size();  //calculating average
-	        
-	        if(RTTList.size() > 0) {
-	                //Printing the min, max, and average of the RTTs
-	        		System.out.format("\r\nMinimum RTT: %.12f, Maximum RTT: %.12f, Average RTT: %.12f\r\n", min, max, avg);
-	        } else System.out.print("\r\nNo connections made.\r\n");
-	        
-	        socket.close(); //closing socket
+            double min = RTTList.get(0);    //starting the comparison w/ the first element
+            double max = RTTList.get(0);    //starting the comparison w/ the first element
+            double avg = 0;
+
+            //going through the list to calculate the min, max, and sum of all the RTT's
+            for(double d : RTTList) {
+                if(d < min) min = d;
+                if(d > max) max = d;
+                avg += d;   //summing all the RTT's so we can find the average
+            }
+            avg /= RTTList.size();  //calculating average
+
+            if(RTTList.size() > 0)
+                //Printing the min, max, and average of the RTTs
+                System.out.format("\r\nMinimum RTT: %.12f, Maximum RTT: %.12f, Average RTT: %.12f", min, max, avg);
+            else
+                System.out.print("\r\nNo connections made.");
+
+            socket.close(); //closing socket
         }
+        //catches exceptions thrown for missing command line arguments
+        catch(ArrayIndexOutOfBoundsException e) {
+            System.out.println("Need two arguments.");
+        }
+        //catches all other exceptions
         catch (Exception e){
             System.out.println(e.getMessage());
         }
